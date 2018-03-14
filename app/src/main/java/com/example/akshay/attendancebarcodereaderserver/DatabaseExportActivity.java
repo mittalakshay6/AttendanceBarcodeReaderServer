090fc83bc5dde789d44633dcb27f3bdb4e200329
Akshay Mittal <mittalakshay6@gmail.com>
2018-03-14 22:00:08 +0530

    More fun in activities

--- app/src/main/java/com/example/akshay/attendancebarcodereaderserver/DatabaseExportActivity.java
+++ app/src/main/java/com/example/akshay/attendancebarcodereaderserver/DatabaseExportActivity.java
@@ -2,12 +2,23 @@ package com.example.akshay.attendancebarcodereaderserver;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class DatabaseExportActivity extends AppCompatActivity {
+
+    private EditText fileNameView;
+    private Button exportBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_database_export);
+
+        fileNameView=findViewById(R.id.fileNameView);
+        exportBtn=findViewById(R.id.exportBtn);
+    }
+    public void onClickExportBtn(View view){
     }
 }
--- app/src/main/java/com/example/akshay/attendancebarcodereaderserver/DatabaseImportActivity.java
+++ app/src/main/java/com/example/akshay/attendancebarcodereaderserver/DatabaseImportActivity.java
@@ -2,12 +2,31 @@ package com.example.akshay.attendancebarcodereaderserver;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class DatabaseImportActivity extends AppCompatActivity {
+
+    TextView pathView;
+    Button browseBtn;
+    Button importDatabaseBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_database_import);
+
+        pathView = findViewById(R.id.pathView);
+        browseBtn = findViewById(R.id.browseBtn);
+        importDatabaseBtn = findViewById(R.id.importBtn);
+
+        importDatabaseBtn.setVisibility(View.INVISIBLE);
+    }
+
+    public void onClickBrowseBtn(View view){
+    }
+
+    public void onClickImportDatabaseBtn(View View){
     }
 }
